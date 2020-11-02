a64fea2a5d9c2cf71b63423feb078fcff824858a
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-02 06:05:15 -0500

    OOPpart4 completed

--- ObjectOrientedProgramming/OOPpart4.py
+++ ObjectOrientedProgramming/OOPpart4.py
@@ -10,9 +10,53 @@ class Point():
         self.x += x
         self.y += y
 
+    # Overload __dunder__
+    def __add__(self, p):
+        return Point(self.x + p.x, self.y + p.y)
+
+    def __sub__(self, p):
+        return Point(self.x - p.x, self.y - p.y)
+    
+    def __mul__(self, p):
+        return Point(self.x * p.x, self.y * p.y)
+
+    def length(self):
+        import math
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def __gt__(self, p):
+        return self.length() > p.length()
+
+    def __ge__(self, p):
+        return self.length() >= p.length()
+    
+    def __lt__(self, p):
+        return self.length() < p.length()
+
+    def __le__(self, p):
+        return self.length() <= p.length()
+
+    def __eq__(self, p):
+        # this math does not alway work out correctly remeber float comparisions
+        #return self.length() == p.length
+        return self.x == p.x and self.y == p.y
+
+
+    # need to __str__ to represent the output of #overloaded
+    def __str__(self):
+        return "(" + str(self.x) + "," + str(self.y) + ")"
+
+
 p1 = Point(3,4)
 p2 = Point(3,2)
 p3 = Point(1,3)
 p4 = Point(0,1)
+p5 = p1 + p2
+p6 = p4 - p1
+p7 = p2 * p3
 
-#3:05:16 https://www.youtube.com/watch?v=sxTmJE4k0ho&t=14178s
+print(p5, p6, p7)
+
+print(p1 == p2)
+print(p1 > p2)
+print(p4 <= p3)
